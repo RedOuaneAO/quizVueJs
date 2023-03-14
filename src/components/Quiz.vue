ba5549a91db846d636b2fee7ed592969e19e2c51
RedOuaneAO <redouaneao97@gmail.com>
2023-03-14 22:49:13 +0100

    changing the visibilite of next button and coloring the answers

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -6,22 +6,29 @@ const selectedAnswer = ref('')
 const correctAnswers = ref(0)
 const selectAnswer = (option) => {
         selectedAnswer.value = option
+        if (option === Questions[currentIndex.value].answer) {
+          correctAnswers.value++
+        }
+        let isRight = document.querySelectorAll(".isright");
+        isRight.forEach(op=>{
+          if(op.textContent == Questions[currentIndex.value].answer){
+            op.classList.add("right")
+          }else {
+            op.classList.add("wrong")
+          }
+        })
       }
   
 const nextQuestion = () => {
-        if (selectedAnswer.value === Questions[currentIndex.value].answer) {
-          correctAnswers.value++
-        }
         currentIndex.value++
         selectedAnswer.value = ''
       }
   
 const showResult = () => {
-        if (selectedAnswer.value === Questions[currentIndex.value].answer) {
-          correctAnswers.value++
-        }
         alert(`You got ${correctAnswers.value} out of ${Questions.length} questions correct!`)
       }
+
+
 
 </script>
 <template>
@@ -36,14 +43,14 @@ const showResult = () => {
             </div>
             <div>
                 <ul class="list-group">
-                    <li class="list-group-item" v-for="option in Questions[currentIndex].options" :key="option" @click="selectAnswer(option)">
+                    <li class="list-group-item isright" v-for="option in Questions[currentIndex].options" :key="option" @click="selectAnswer(option)" >
                     {{ option }}
                     </li>
                 </ul>
 
                 <div class="mt-5 d-flex justify-content-evenly">
-                    <button class="btn bg-success text-white px-3" v-if="currentIndex < Questions.length - 1" @click="nextQuestion">next</button>
-                    <button class="btn bg-success text-white px-3" v-else @click="showResult">Submit</button>
+                    <button class="btn bg-success text-white px-3" :disabled="!selectedAnswer"  v-if="currentIndex < Questions.length - 1" @click="nextQuestion">next</button>
+                    <button class="btn bg-success text-white px-3" :disabled="!selectedAnswer"  v-else @click="showResult">Submit</button>
                 </div>
             </div>
         </div>
@@ -65,6 +72,13 @@ li{
     width: 10%;
     height: 10px;
     border-radius: 5px;
+  }
+  .right {
+background-color: green;
+  }
+  .wrong{
+    background-color: red;
+
   }
 
 </style>
